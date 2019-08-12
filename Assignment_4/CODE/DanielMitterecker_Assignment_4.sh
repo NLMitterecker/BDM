9bd5022dd36626a1db741cbea4d1fcf9ac6d7f28
User <dmr244@packer-Ubuntu-18.04.2-Server>
2019-08-12 21:14:22 +0200

    further work on bash prototype

--- Assignment_4/CODE/DanielMitterecker_Assignment_4.sh
+++ Assignment_4/CODE/DanielMitterecker_Assignment_4.sh
@@ -1,33 +1,29 @@
 #!/usr/bin/env bash
-
-fileName=$1
+filename=$1
 data_output_path="../OUTPUT"
+data_input_path="../INPUT"
 r_plot_script="./Plot_Zs.R"
 r_add_z_script="./add_Zs.R"
 
 function remove_dups {
-	local fileName=$1
-	local output_filename="$data_output_path/Nodups_$fileName"
-	listOfDuplicates=$(awk '{print $1}' Z1_Z2_SWB_Full_v2.txt | sort -k1 | uniq -D | uniq | paste -sd "|" -)
-	sed "/$listOfDuplicates/d" $fileName > $output_filename
-	echo $output_filename
+	local input_data=$1
+	local output_data="$data_output_path/Nodups_$filename"
+	duplicated_ids=$(awk '{print $1}' $input_data | sort -k1 | uniq -D | uniq | paste -sd "|" -)
+	awk -v duplicates_regex_string=$duplicated_ids -v OFS="\t" '$1 !~ duplicates_regex_string' $input_data > $output_data
+	echo $output_data
 }
 
 
-
-function main {
-	if [[ $# < 1 ]]; then
-		echo "Error: No file name has been supplied."
+if [[ $# < 1 ]]; then
+	echo "Error: No file name has been supplied."
 		exit
-	elif [[ $# > 1 ]]; then
-		echo "Error: You have supplied more than one file names. Please specify only one."
-		exit
-	else 
-		echo "The script is being executed"
-		$output_filename=$(remove_dups $fileName)
-		$r_add_z_script $output_filename
-		$r_plot_script $output_filename
-	fi
-}
+elif [[ $# > 1 ]]; then
+	echo "Error: You have supplied more than one file names. Please specify only one."
+	exit
+else 
+	echo "The script is being executed"
+	output_data=$(remove_dups $data_input_path/$filename)
+	#$r_add_z_script $output_filename
+	#$r_plot_script $output_filename
+fi
 
-main
